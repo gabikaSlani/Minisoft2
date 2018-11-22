1698c6703e1759e17a6575804402e3ab6c77c071
spisakova31 <andreja.spisakova@gmail.com>
2018-11-22 16:24:42 +0100

    first commit

---
+++ Assets/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public Text levelText;
+    public Text scoreText;
+    public Button buttonCarrot;
+    public Button buttonForward;
+    public Button buttonPlay;
+
+    int level;
+
+	// Use this for initialization
+	void Start () {
+        level = 1;
+        levelText.text = "" + level;
+        scoreText.text = "0/0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            levelText.text = "" + level++;
+        }
+	}
+}
